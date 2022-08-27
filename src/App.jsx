5bbd3cacdea7cf52a636c7bedd9b900993004343
paraschivcristea <100124467+paraschivcristea@users.noreply.github.com>
2022-08-27 19:10:15 +0300

    third commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import { useState } from 'react'
+import Navigation from "./components/Navigation"
 import './App.css'
 
 function App() {
   return (
     <div className="App">
-      Test
+      <Navigation />
     </div>
   )
 }
